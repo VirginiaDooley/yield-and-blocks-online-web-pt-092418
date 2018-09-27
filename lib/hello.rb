b589e317124ca346a3ebd599416930c9df97af2d
Virginia Dooley <vcdooley@gmail.com>
2018-09-27 11:12:34 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,3 @@
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-   if name.start_with?("T")
-     puts "Hi, #{name}"
-   end
-end
-
 def hello_t(array)
   i = 0 
   
@@ -13,4 +7,9 @@ def hello_t(array)
   end
 end
 
-hello_t
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+   if name.start_with?("T")
+     puts "Hi, #{name}"
+   end
+end
+
