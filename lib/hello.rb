deaf16d0aec1127bd9ede063e98fbe2bf4a14e26
Virginia Dooley <vcdooley@gmail.com>
2018-09-27 11:09:18 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,7 +1,14 @@
+# ["Tim", "Tom", "Jim"].each do |name|
+#   if name.start_with?("T")
+#     puts "Hi, #{name}"
+#   end
+# end
+
 def hello_t(array)
   i = 0 
   
   while i < array.length 
+    yield array[i]
     i = i + 1
   end
 end
